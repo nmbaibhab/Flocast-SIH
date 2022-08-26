d4eb94300cb5a5d0de0e6f6f516a5579b353711a
Ash-exp <asutosh2000ad@gmail.com>
2022-08-26 15:57:35 +0530

    user login

--- src/components/Auth/UserLogin.jsx
+++ src/components/Auth/UserLogin.jsx
@@ -13,7 +13,7 @@ const validateForm = (errors) => {
   return valid;
 };
 
-export default class DistLogin extends React.Component {
+export default class UserLogin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -74,12 +74,12 @@ export default class DistLogin extends React.Component {
               },
             }
           )
-          .then(function (response) {
+          .then(function(response) {
             console.log(response.data);
             navigate("/sendFloodMsg");
             localStorage.setItem("userProfile", response.data);
           })
-          .catch(function (error) {
+          .catch(function(error) {
             console.log(error);
             Swal.fire({
               icon: "error",
@@ -104,7 +104,7 @@ export default class DistLogin extends React.Component {
               <div className="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-6 md:mb-0">
                 <img
                   // src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                  src={DistImage}
+                  src={DistImg}
                   className="w-full"
                   alt="Sample image"
                 />
@@ -120,68 +120,67 @@ export default class DistLogin extends React.Component {
                     </p>
                   </div>
 
-                <div className="flex items-center my-4 before:flex-1 before:border-t before:border-blue-700 before:mt-0.5 after:flex-1 after:border-t after:border-blue-700 after:mt-0.5">
-                  {/* <p className="text-center font-semibold mx-4 mb-0 text-green-700">
+                  <div className="flex items-center my-4 before:flex-1 before:border-t before:border-blue-700 before:mt-0.5 after:flex-1 after:border-t after:border-blue-700 after:mt-0.5">
+                    {/* <p className="text-center font-semibold mx-4 mb-0 text-green-700">
                     Hello Again !!
                   </p> */}
-                </div>
-
-                <div className="my-6">
-                  <input
-                    type="email"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="email"
-                    placeholder="Email address"
-                    required
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-
-                <div className="mb-6">
-                  <input
-                    type="password"
-                    className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-                    id="password"
-                    placeholder="Password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-
-                <div className="flex justify-between items-center mb-6">
-                  <div className="form-group form-check">
-                    <input
-                      type="checkbox"
-                      className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                      id="exampleCheck2"
-                    />
-                    <label
-                      className="form-check-label inline-block text-gray-800"
-                      for="exampleCheck2"
-                    >
-                      Remember me
-                    </label>
                   </div>
-                  <a href="#!" className="text-blue-700">
-                    Forgot password?
-                  </a>
-                </div>
 
-                <div className="text-center lg:text-left">
-                  <button
-                    type="submit"
-                    className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-800 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  >
-                    Login
-                  </button>
-                </div>
-              </form>
+                  <div className="my-6">
+                    <input
+                      type="email"
+                      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="email"
+                      placeholder="Email address"
+                      required
+                      onChange={(e) => this.setEmail(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="mb-6">
+                    <input
+                      type="password"
+                      className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                      id="password"
+                      placeholder="Password"
+                      required
+                      onChange={(e) => this.setPassword(e.target.value)}
+                    />
+                  </div>
+
+                  <div className="flex justify-between items-center mb-6">
+                    <div className="form-group form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                        id="exampleCheck2"
+                      />
+                      <label
+                        className="form-check-label inline-block text-gray-800"
+                        for="exampleCheck2"
+                      >
+                        Remember me
+                      </label>
+                    </div>
+                    <a href="#!" className="text-blue-700">
+                      Forgot password?
+                    </a>
+                  </div>
+
+                  <div className="text-center lg:text-left">
+                    <button
+                      type="submit"
+                      className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-800 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      Login
+                    </button>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </div>
-  );
-};
-
-export default GovLogin;
+        </section>
+      </div>
+    );
+  }
+}
